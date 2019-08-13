2ffdda88dd192bc32304752098b1d8904fd9d020
Tony Baker <this.tonybaker@gmail.com>
2019-08-13 21:31:21 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_selected(song, nested)
+  def artist_select(song, nested)
     if nested
       hidden_field_tag "song[artist_id]", song.artist_id
     else
